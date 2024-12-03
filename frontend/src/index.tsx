6b0cf86db31527b3e570745bdb6a155a50b7bb85
keshav jha <keshavjhaa2678@gmail.com>
2024-12-03 23:18:42 +0530

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,9 @@
-// src/index.tsx
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import client from './services/apolloClient'; 
 
-// Entry point for React
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <ApolloProvider client={client}>
@@ -14,5 +11,4 @@ root.render(
   </ApolloProvider>,
 );
 
-// Add this line to ensure the file is treated as a module
 export {};
